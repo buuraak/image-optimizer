316b4db1ba2909cba9f33bbe4ea71d1c13344165
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-05-23 10:33:57 +0200

    UI: Create header component & styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import './../styles/globals.css'
+import "@uploadthing/react/styles.css";
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
---
+++ src/components/IntroHeader.tsx
@@ -0,0 +1,23 @@
+import Uploader from "./Uploader";
+
+const IntroHeader = () => {
+    return (
+        <header className="flex justify-center flex-col item-center mx-auto min-h-screen">
+            <div>
+                <h1 className="font-extrabold lg:max-w-[25ch] m-auto  text-2xl sm:text-4xl lg:text-5xl text-center tracking-tight leading-tight">
+                    Boost Your Website's Performance with Image Optimization
+                </h1>
+                <p className="tracking-tight leading-tight text-md pt-5">
+                    Supercharge loading speeds with optimized images, delivering
+                    a seamless user experience.
+                </p>
+            </div>
+            <div className="pt-5">
+                <Uploader />
+            </div>
+
+        </header>
+    );
+};
+
+export default IntroHeader;
