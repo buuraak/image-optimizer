a9b4f75bbd156a0d22a5a97bbcd0c70abaa04dc8
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-05-25 10:55:12 +0200

    Uninstall uploadthing api

--- src/app/api/uploadthing/route.ts
+++
@@ -1,8 +0,0 @@
-/** app/api/uploadthing/route.ts */
-import { ourFileRouter } from "./core";
-import { createNextRouteHandler } from "uploadthing/next";
- 
-// Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-});
