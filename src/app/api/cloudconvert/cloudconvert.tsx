0bf8012f7303909f91992be9e38edc057d627fbe
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-05-26 20:16:17 +0200

    Comment out cloudconvert.tsx

--- src/app/api/cloudconvert/cloudconvert.tsx
+++ src/app/api/cloudconvert/cloudconvert.tsx
@@ -1,38 +1,38 @@
 import CloudConvert from "cloudconvert";
 
-const cloudConvert = new CloudConvert(process.env.NEXT_PUBLIC_CLOUDCONVERT_SECRET);
+//const cloudConvert = new CloudConvert(process.env.NEXT_PUBLIC_CLOUDCONVERT_SECRET);
 
-export const OptimizeFile = async (fileName: string, url: string) => {
+//export const OptimizeFile = async (fileName: string, url: string) => {
     // Create a new job for the image
-    let job = await cloudConvert.jobs.create({
-        "tasks": {
-            "import": {
-                "operation": "import/url",
-                "url": url,
-                "filename": fileName
-            },
-            "optimize": {
-                "operation": "optimize",
-                "input": [
-                    "import"
-                ]
-            },
-            "export-optimized": {
-                "operation": "export/url",
-                "input": [
-                    "optimize"
-                ],
-                "inline": false,
-                "archive_multiple_files": false
-            }
-        },
-        "tag": "jobbuilder"
-    });
-    // Wait asynchronously for the job to complete
-    job = await cloudConvert.jobs.wait(job.id);
-
+//    let job = await cloudConvert.jobs.create({
+//        "tasks": {
+//            "import": {
+//                "operation": "import/url",
+//                "url": url,
+//                "filename": fileName
+//            },
+//            "optimize": {
+//                "operation": "optimize",
+//                "input": [
+//                    "import"
+//                ]
+//            },
+//            "export-optimized": {
+//                "operation": "export/url",
+//                "input": [
+//                    "optimize"
+//                ],
+//                "inline": false,
+//                "archive_multiple_files": false
+//            }
+//        },
+//        "tag": "jobbuilder"
+//    });
+//    // Wait asynchronously for the job to complete
+//    job = await cloudConvert.jobs.wait(job.id);
+//
     //Export the temporary url
-    const file = cloudConvert.jobs.getExportUrls(job)[0];
-
-    return file;
-}
+//    const file = cloudConvert.jobs.getExportUrls(job)[0];
+//
+//   return file;
+//}
