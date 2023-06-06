1035397a93a02068efe703d6eb5b5fe1ad504dd5
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-06-07 00:55:18 +0300

    Add: Backdrop image as background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import IntroSection from "../components/IntroHeader";
+import Backdrop from "../components/Backdrop";
 export default function Home() {
     return (
             <main className="mx-auto container text-center px-6">
                 <IntroSection />
+                <Backdrop />
             </main>
     );
 }
---
+++ src/components/Backdrop.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export default function Backdrop() {
+    return (
+        <Image 
+            src="/backdrop.webp"
+            alt="backdrop"
+            width={1920}
+            height={1080}
+            sizes='100vw'
+            className='absolute h-full w-full top-0 left-0 z-[-1] object-cover'
+        />
+    )
+}
