f7912aa284ece5fa09457fad4308abae5b4f22ac
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-06-22 11:13:27 +0200

    Format: s3 api route.ts

--- src/app/api/s3/route.ts
+++ src/app/api/s3/route.ts
@@ -1,52 +1,50 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextResponse } from "next/server";
 
-const ONE_HOUR: number = (60 * 60);
-
+const ONE_HOUR: number = 60 * 60;
 /*
-    * Right now typescript gives an error because the process.env type returns 'string | undefined' which does not match the credentials typescript 
-*/
-const AWS_ACCESS_KEY = process.env.ACCESS_KEY ?? 'default'; 
-const AWS_SECRET_KEY = process.env.SECRET_KEY ?? 'default';
+ * Right now typescript gives an error because the process.env type returns 'string | undefined' which does not match the credentials typescript
+ */
+const AWS_ACCESS_KEY = process.env.ACCESS_KEY ?? "default";
+const AWS_SECRET_KEY = process.env.SECRET_KEY ?? "default";
 
 export async function POST(request: Request) {
-    if(request.method !== "POST") NextResponse.json({message: "Only POST method allowed"}, {status: 405});
+    if (request.method !== "POST")
+        NextResponse.json(
+            { message: "Only POST method allowed" },
+            { status: 405 }
+        );
     const s3 = new S3Client({
-    region: process.env.REGION,
-    credentials: {
-        accessKeyId: AWS_ACCESS_KEY,
-        secretAccessKey: AWS_SECRET_KEY
-    },
-});
-
+        region: process.env.REGION,
+        credentials: {
+            accessKeyId: AWS_ACCESS_KEY,
+            secretAccessKey: AWS_SECRET_KEY,
+        },
+    });
 
     try {
-        const formData  = await request.formData();
-        const file: any = formData.get('file');
-        
+        const formData = await request.formData();
+        const file: any = formData.get("file");
         const fileParams = {
             Bucket: process.env.BUCKET_NAME,
             Key: file.name,
             ContentType: file.type,
         };
         const command = new PutObjectCommand(fileParams);
-        const url = await getSignedUrl(s3, command, {expiresIn: ONE_HOUR});
-        
-        const response = await fetch(url, {
-            method: 'PUT',
+        const url = await getSignedUrl(s3, command, { expiresIn: ONE_HOUR });
+
+        await fetch(url, {
+            method: "PUT",
             headers: {
                 "Content-type": file.type,
                 "Access-Control-Allow-Origin": "*",
             },
-            body: file
+            body: file,
         });
-        return NextResponse.json({message: 'success', fileName: file.name}, {status:200});
-
-    } catch(error) {
+        return NextResponse.json({ fileName: file.name }, { status: 200 });
+    } catch (error) {
         console.log(error);
-        NextResponse.json({ message: error  }, {status: 400});
+        NextResponse.json({ message: error }, { status: 400 });
     }
 }
-
-
