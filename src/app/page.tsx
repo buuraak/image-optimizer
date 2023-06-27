661b4641563b117e9277e14caf67c0234e4e2a6b
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-06-27 12:52:38 +0200

    Add: Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import IntroSection from "../components/IntroHeader";
 import Backdrop from "../components/Backdrop";
 import { Inter } from "next/font/google";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main
-            className={`mx-auto container text-center px-6 ${inter.className}`}
-        >
+        <main className={`mx-auto container text-center px-6 ${inter.className}`}>
+            <Header />
             <IntroSection />
             <Backdrop />
         </main>
--- src/components/IntroHeader.tsx
+++ src/components/IntroHeader.tsx
@@ -2,7 +2,7 @@ import Uploader from "./Uploader";
 
 const IntroHeader = () => {
     return (
-        <header className="flex justify-center flex-col item-center mx-auto min-h-screen">
+        <div className="flex justify-center flex-col item-center mx-auto min-h-screen">
             <div>
                 <h1 className="font-extrabold lg:max-w-[25ch] m-auto  text-2xl sm:text-4xl lg:text-5xl text-center tracking-tight leading-tight">
                     Boost Your Website's Performance with Image Optimization
@@ -15,7 +15,7 @@ const IntroHeader = () => {
             <div className="pt-5">
                 <Uploader />
             </div>
-        </header>
+        </div>
     );
 };
 
