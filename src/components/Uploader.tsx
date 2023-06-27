1999260a35aa9acbd3786ba64954f29df4b535bf
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-06-27 12:58:18 +0200

    Fix: Wrong POST url

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -58,7 +58,7 @@ const Uploader = () => {
         try {
             toast.update(id, { render: "Compressing file" });
             const compressorResponse = await fetch(
-                "https://cloudconverter.vercel.app/compress-image/compress-image",
+                "https://cloudconverter.vercel.app/compress-image",
                 {
                     method: "POST",
                     headers: {
